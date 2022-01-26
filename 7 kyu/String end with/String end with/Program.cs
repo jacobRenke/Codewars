102d5f75c0db4230799924f6b3ccaab54d26cfa2
Jacob Renke <jacob.m.renke@outlook.com>
2022-01-25 21:03:52 -0500

    success

---
+++ 7 kyu/String end with/String end with/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace String_end_with
+{
+    internal class Program
+    {
+        public static void Main(){
+        Console.WriteLine(Solution("Fart", "art"));
+        Console.WriteLine(Solution("Cinnamon", "n"));
+        Console.WriteLine(Solution("Ribosome", "ygfdsgdfghdsfgdsfgdsfg"));
+        }
+        
+
+        public static bool Solution(string str, string ending)
+        {
+            if (str.Length >= ending.Length)
+            {
+                int diff = str.Length - ending.Length;
+                string firstWord = str.Substring(diff, ending.Length);
+                string secondWord = ending.Substring(0, ending.Length);
+                if (firstWord == secondWord)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+}
+
+
+
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+//Examples:
+
+//solution('abc', 'bc') // returns true
+//solution('abc', 'd') // returns false
