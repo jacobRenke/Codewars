f1eb5d69037576c844e5b06196ab2c3f599d362d
Jacob Renke <jacob.m.renke@outlook.com>
2022-02-15 01:35:01 -0500

    complete

---
+++ 6 kyu/Find the unique number/Find the unique number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Find_the_unique_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> arr = new int[] { -2, 2, 2, 2 };
+            Console.WriteLine(GetUnique(arr));
+        }
+
+        public static int GetUnique(IEnumerable<int> numbers)
+        {
+            int[] findNumber = numbers.ToArray<int>();
+            Array.Sort(findNumber);
+            int answer = 0;
+
+            for (int i = 0; i < findNumber.Length - 1; i++)
+            {
+                for (int j = 0; j < findNumber.Length; j++)
+                {
+                    if (findNumber[i] != findNumber[j])
+                    {
+                        answer = findNumber[j];
+                    }
+                }
+            }
+            return answer;
+        }
+
+
+    }
+}
