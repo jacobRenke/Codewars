f9a6bfd39566cac63e0d796214877ceef5822604
Jacob Renke <jacob.m.renke@outlook.com>
2022-02-20 13:15:41 -0500

    complete

---
+++ 8kyu/Calculate BMI/Calculate BMI/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+double weight = 80;
+double height = 1.80;
+Console.WriteLine(Bmi(weight, height));
+
+static string Bmi(double weight, double height)
+{
+    string result = "";
+    double BMI = (weight / (Math.Pow(height, 2)));
+
+    if (BMI <= 18.5)
+    {
+        result = "Underweight";
+    }
+    else if (BMI <= 25)
+    {
+        result = "Normal";
+    }
+    else if (BMI <= 30)
+    {
+        result = "Overweight";
+    }
+    else if (BMI > 30)
+    {
+        result = "Obese";
+    }
+    return result;
+    
+}
+
