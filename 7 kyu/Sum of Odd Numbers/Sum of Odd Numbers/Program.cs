14f504cf09f3d5036358a1ffb94fe784b75494c8
Jacob Renke <jacob.m.renke@outlook.com>
2022-02-21 23:49:02 -0500

    complete

---
+++ 7 kyu/Sum of Odd Numbers/Sum of Odd Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+long n = 2;
+Console.WriteLine(rowSumOddNumbers(n));
+
+
+static long rowSumOddNumbers(long n)
+{
+    long result = 0;
+
+    //loop for how many times I need to go through pyramid
+    long iterations = 0;
+    for (int i = 1; i <= n * n - 1; i += 2)
+    {
+        iterations += i;
+    }
+    
+
+
+    if (n == 1)
+        return 1;
+    
+    for (long j = 1; j < iterations; j += 2)
+    {
+        result += j;
+    }
+
+    return result;
+}
