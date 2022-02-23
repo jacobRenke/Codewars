2df530fded5825fb968eb88012286f4b0cd018ec
Jacob Renke <jacob.m.renke@outlook.com>
2022-02-23 14:54:30 -0500

    complete

--- 7 kyu/Sum of Odd Numbers/Sum of Odd Numbers/Program.cs
+++ 7 kyu/Sum of Odd Numbers/Sum of Odd Numbers/Program.cs
@@ -9,21 +9,24 @@ static long rowSumOddNumbers(long n)
     long result = 0;
 
     //loop for how many times I need to go through pyramid
-    long iterations = 0;
-    for (int i = 1; i <= n * n - 1; i += 2)
-    {
-        iterations += i;
-    }
-    
+    //long iterations = 0;
+    //for (int i = 1; i <= n * n - 1; i += 2)
+    //{
+    //    iterations += i;
+    //}
 
 
-    if (n == 1)
-        return 1;
-    
-    for (long j = 1; j < iterations; j += 2)
-    {
-        result += j;
-    }
 
+    //if (n == 1)
+    //    return 1;
+
+    //for (long j = 1; j < iterations; j += 2)
+    //{
+    //    result += j;
+    //}
+
+    //return result;
+
+    result = (n * n) * n;
     return result;
 }
